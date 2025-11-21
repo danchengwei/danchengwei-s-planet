19b0015b13625f9a8c8a32cc9aac630aaee56974
但成伟 <w_danchengwei7@xiwang.com>
2025-11-21 16:52:12 +0800

    feat:vite代理修改

--- OthersWebRtcTest/vite.config.js
+++ OthersWebRtcTest/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // 允许局域网访问
     proxy: {
       // 配置代理
       '/api': {
