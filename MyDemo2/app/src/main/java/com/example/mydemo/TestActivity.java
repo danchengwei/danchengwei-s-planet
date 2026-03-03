b03eeff1ec437e81c9ba5220b259101398439cca
danchengwei <danchengwei@tal.com>
2026-03-03 11:44:43 +0800

    feat：测试

--- MyDemo2/app/src/main/java/com/example/mydemo/TestActivity.java
+++ MyDemo2/app/src/main/java/com/example/mydemo/TestActivity.java
@@ -3,6 +3,7 @@ package com.example.mydemo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -64,5 +65,13 @@ public class TestActivity extends Activity {
     @Override
     protected void onStop() {
         super.onStop();
+        Log.d("TestActivity", "onStop");
+        Log.d("TestActivity", "onStop");
+        Log.d("TestActivity", "onStop");
+        Log.d("TestActivity", "onStop");
+        Log.d("TestActivity", "onStop");
+        Log.d("TestActivity", "onStop");
+
     }
+
 }
